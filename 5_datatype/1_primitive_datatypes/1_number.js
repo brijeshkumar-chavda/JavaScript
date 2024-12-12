7d785f6ec98bc12fccece149ecd95dc2de3393a3
Brijeshkumar Chavda <chavdabrijeshkumar25@gmail.com>
2024-12-12 20:54:52 +0530

    refactor: numbers

--- 5_datatype/1_primitive_datatypes/1_number.js
+++ 5_datatype/1_primitive_datatypes/1_number.js
@@ -17,17 +17,16 @@
 // Number
 let integer = 123;
 let float = 3.14;
-let scientificNumber = 10e2;
+let scientific = 10e2;
 
 // BigInt: BigInt numbers can only perform operation with BigInt datatype
-
 // Creating BigInt number using BigInt() constructor
 let bigInt1 = BigInt(1000);
 
 // Creating BigInt number using `n` at the end of the number
 let bigInt2 = 1000n;
 
-// NaN: represent invalid value
+// NaN (Not a number): represent invalid value
 let notANumber = NaN;
 
 // Infinity: represent  infinite numeric values
