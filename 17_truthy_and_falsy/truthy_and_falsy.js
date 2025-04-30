cd68baf689b5a29a94bb1fe030da48fd52636aad
Brijeshkumar Chavda <chavdabrijeshkumar25@gmail.com>
2025-04-30 14:22:26 +0530

    refactor: truthy and falsy value

--- 17_truthy_and_falsy/truthy_and_falsy.js
+++ 17_truthy_and_falsy/truthy_and_falsy.js
@@ -1,46 +1,19 @@
 // Truthy VS Falsy
 
-// Falsy value: Any value which is treated as a false. Known as a falsy value.
-// false
-// 0
-// -0
-// BigInt 0n
-// Empty string: ""
+// Falsy: Any value which is treated as a false. Known as a falsy value.
+// false (Boolean literal)
+// 0 (Zero)
+// -0 (Negative zero)
+// 0n (BigInt zero)
+// "" (Empty string)
 // null
 // undefined
-// NaN
+// NaN (Not-a-Number)
 
-if (0) {
-  console.log("code executed");
-}
-
-// Truthy value: Any value other than falsy.
-// "0"
-// "false"
-// String with white space: "    "
+// Truthy: Any value other than falsy.
+// true (Boolean literal)
+// " " (Any non-empty string)
+// 123 (Any non-zero number)
 // []
 // {}
 // function(){}
-
-const emptyArray = [];
-if (emptyArray.length === 0) {
-  console.log("Array is empty");
-}
-
-const emptyObject = {};
-if (Object.keys(emptyObject).length === 0) {
-  console.log("Objet is empty");
-}
-
-// For Interview
-// false == 0 -> true
-// false == -0 -> true
-// false == 0n -> true
-// false == "0" -> true
-// false == "" -> true
-
-// false == null -> false
-// false == undefine -> false
-// false == NaN -> false
-const result = false == NaN;
-console.log(result);
