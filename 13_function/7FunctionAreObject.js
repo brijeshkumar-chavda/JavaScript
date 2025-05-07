128d24ea9f67af979240ff7e91db569271e6f595
Brijeshkumar Chavda <chavdabrijeshkumar25@gmail.com>
2025-05-07 19:52:50 +0000

    add: function are object

--- 13_function/7FunctionAreObject.js
+++ 13_function/7FunctionAreObject.js
@@ -1,28 +1,39 @@
 // Function Are Object
 
-// Q. What is function are object?
-// - When you use `typeof` operator to the datatype of function you will get function back. But internally function are work same as an object. It has method and property same as an object has.
+// What
+// - In JavaScript, a function is a special type of object. It's not just a block of code that executes; it also has properties and methods, just like regular JavaScript objects. You can think of a function as an object that is also callable.
 
-// NOTE
-// - To see function as a object use
-// console.dir(function_name);
+// Why
+// Treating functions as objects provides several powerful capabilities:
+// First-Class Citizens: This is the most important reason. It means functions can be:
+// - Assigned to variables.
+// - Passed as arguments to other functions (callbacks).
+// - Returned from other functions (higher-order functions).
+// - Stored in data structures (arrays, objects).
+// Adding Properties: You can attach custom properties to a function, just like you can with any object. This allows you to associate metadata or related values directly with the function.
+// Methods: Functions also have built-in methods like call(), apply(), and bind(), which are crucial for controlling how a function is executed and what this value it uses.
+// Prototype: Every function in JavaScript has a prototype property (which is itself an object). This is the foundation of JavaScript's prototypal inheritance. Objects created using the new keyword with a function (as a constructor) will inherit properties and methods from this prototype object.
 
-// To remove the confusion between function and object here the example
 
-// Object
-let object = {
-  key: "value",
-};
+// How
+// - When a function is created, the JavaScript engine creates a function object. This object contains:
+// - The executable code of the function.
+// - A set of built-in properties (like name, length, and prototype).
+// - Internal methods that handle how the function is called and behaves.
 
-console.log(object.key); // Accessing key/ value
-
-object.key_1 = "value_1"; // Adding key/ value
-console.log(object);
-
-// Function
-function function_() {
-  console.log("Hello, World!");
+// Example
+function greet(name) {
+  console.log(`Hello, ${name}!`);
 }
-console.log(function_.length);
-console.log(function_.name);
-console.log(function_.toString());
+
+greet.language = "English";
+console.log(greet.language); // Output: English
+
+greet.explain = function() {
+  console.log(`This greeting is in ${this.language}.`);
+};
+greet.explain(); // Output: This greeting is in English.
+
+// Note
+// - While functions behave like objects, there's a subtle distinction. They have an internal [[Call]] method that allows them to be executed, which regular objects don't have. The typeof operator reflects this special nature by returning "function" for functions, even though they are fundamentally objects.
+// - To see function as a object use `console.dir(function_name)`. It will show all the properties of that function in object form
