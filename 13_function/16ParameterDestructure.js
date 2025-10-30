3cf550a41f8a10a7671a8652d9595af19c58d8f4
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-10-30 12:52:04 +0530

    refactor: parameter destructure

--- 13_function/16ParameterDestructure.js
+++ 13_function/16ParameterDestructure.js
@@ -1,40 +1,47 @@
-// Parameter Destructuring
+// Topic: Parameter Destructuring
 
-// Q.What is parameter destructuring?
-// - Parameter destructuring in JavaScript is a way to extract values from an object or an array and assign them to variables in a more concise and convenient manner. It allows you to "destructure" the data structure and directly access its individual values without manually accessing them using dot notation or indexing.
+// Q. What is parameter destructuring?
+// -> Parameter destructuring in JavaScript is a way to extract values from an object or an array and assign them to variables in a more concise and convenient manner. It allows you to "destructure" the data structure and directly access its individual values without manually accessing them using dot notation or indexing.
 
-// Q.Why use parameter destructing?
-// - Parameter destructuring simplifies the process of extracting values from objects and arrays, making your code more concise and readable. It eliminates the need for explicit assignments and reduces the amount of code required to access individual values.
+// Q. Why use parameter destructing?
+// -> Parameter destructuring simplifies the process of extracting values from objects and arrays, making your code more concise and readable. It eliminates the need for explicit assignments and reduces the amount of code required to access individual values.
 
-// 1. Object Destructuring:
-// - When a function has an object as a parameter, you can use parameter destructuring to extract specific values from the object.
+// Syntax:
+// 1. Object Destructuring
+// -> When a function has an object as a parameter, you can use parameter destructuring to extract specific values from the object.
 
 // Without destructuring
-function printPerson(person) {
-  var name = person.name;
-  var age = person.age;
+const object = { key1: "value1", key2: "value2" };
 
-  console.log(name + " is " + age + " years old.");``
+function function1(object) {
+  const key1 = object.key1;
+  const key2 = object.key2;
+
+  console.log(key1);
+  console.log(key2);
 }
 
 // With destructuring
-function printPerson({ name, age }) {
-  console.log(name + " is " + age + " years old.");
+function function1({ key1, key2 }) {
+  console.log(key1);
+  console.log(key2);
 }
 
 // 2. Array Destructuring
 
 // Without destructuring
-function printNumbers(numbers) {
-  var firstNumber = numbers[0];
-  var secondNumber = numbers[1];
+const array = [1, 2, 3, 4, 5];
 
-  console.log("The first number is " + firstNumber + ".");
-  console.log("The second number is " + secondNumber + ".");
+function function2(array) {
+  const variable1 = array[0];
+  const variable2 = array[1];
+
+  console.log(variable1);
+  console.log(variable2);
 }
 
 // With destructuring
-function printNumbers([firstNumber, secondNumber]) {
-  console.log("The first number is " + firstNumber + ".");
-  console.log("The second number is " + secondNumber + ".");
+function function2([variable1, variable2]) {
+  console.log(variable1);
+  console.log(variable2);
 }
