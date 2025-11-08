624336d3e52239a7b935bd3df8c67c0f1091c628
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-08 17:54:25 +0530

    refactor: set timeout

--- 23_asynchronous_javascript/02_setTimeout_and_setInterval/setTimeout_and_setInterval.js
+++ 23_asynchronous_javascript/02_setTimeout_and_setInterval/setTimeout_and_setInterval.js
@@ -1,43 +1,41 @@
 // Asynchronous
 
-// What is Asynchronous?
+// What
 // - "I will finish later."
 // - In programming, asynchronous refers to the ability of code to execute independently of the main program flow. In other words, asynchronous operations allow certain tasks to start, run, and complete separately from the rest of the code, without blocking or pausing the execution of the program.
 
 // setTimeout();
 
-// What is setTimeout?
+// What
 // - It is inbuilt JavaScript function which use for executing code after the time limit is finished.
 
-// NOTE
+// Note
 // - delay parameter use millisecond (ms).
 // - 1000ms = 1sec
 
-// SYNTAX
+// Syntax
 // setTimeout(function, delay);
 
-setTimeout(myFunction, 3000); // You can also pass the function directly into the setTimeout();
+setTimeout(function1, 3000);
 
-function myFunction() {
+function function1() {
   console.log("Hello, World");
 }
 
 // setInterval();
 
-// Q. What is setInterval?
+// What
 // - setInterval is a built-in function in JavaScript that repeatedly executes a specified function or code snippet at a fixed time interval. It allows you to schedule the execution of a function repeatedly, with a specified delay between each execution.
 
-// SYNTAX
+// Syntax
 // setInterval(function, delay);
 
-function myFunction() {
+function function1() {
   console.log("Hello");
 }
 
-// setInterval(fun, 1000);
-
 // After 5 seconds, stop the execution
-const intervalId = setInterval(myFunction, 1000);
+const intervalId = setInterval(function1, 1000);
 setTimeout(function () {
   clearInterval(intervalId);
 }, 5000);
